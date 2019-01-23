80756df16909c84a2600c3b5b4f975e385b9c409
Ella Taber <michaella.latkovic@gmail.com>
2019-01-23 04:47:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,8 +10,11 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+var itemObj = {itemName:`${item}`,itemPrice:(Math.floor(Math.random() * 100) + 1)}
+cart.push(itemObj)
+return (`${item} has been added to your cart.`)
 }
+
 
 function viewCart() {
   // write your code here
